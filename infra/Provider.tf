00327465fd731ccbde05fb3eadd4792cfd32d6c9
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 14:54:45 -0300

    Modify provider versions and add new providers
    Updated AWS provider version constraint and added new providers for TLS, time, null, and cloudinit.

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -4,12 +4,32 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.95"
+      version = ">= 5.95.0, < 6.0.0"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.38"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.13"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3"
     }
   }
 }
