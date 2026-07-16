5ab34627bbe16e5ea62e71ed2787e5a37557666c
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 15:53:39 -0300

    Add Kubernetes provider and update service resource

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -1,40 +1,54 @@
 terraform {
+  required_version = ">= 0.14.9"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.47"
     }
-  }
 
-  required_version = ">= 0.14.9"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20.0"
+    }
+  }
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
     command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      var.cluster_name
+    ]
   }
 }
 
-resource "kubernetes_service" "LoadBalancer" {
+resource "kubernetes_service_v1" "LoadBalancer" {
   metadata {
     name = "load-balancer-go-api"
   }
+
   spec {
     selector = {
       nome = "go"
     }
+
     port {
-      port = 8000
+      port        = 8000
       target_port = 8000
     }
+
     type = "LoadBalancer"
   }
 }
