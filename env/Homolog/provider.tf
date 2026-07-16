4350fc0291f8b214706a4b79128645c9bef933f1
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 15:17:08 -0300

    Add AWS provider and homolog module configuration

--- env/Homolog/provider.tf
+++ env/Homolog/provider.tf
@@ -10,3 +10,26 @@ terraform {
   }
 
 }
+
+
+provider "aws" {
+
+  region = "us-east-2"
+
+}
+
+
+module "homolog" {
+
+  source = "../../infra"
+
+  cluster_name = "homolog2"
+
+}
+
+
+output "IP_db" {
+
+  value = module.homolog.IP
+
+}
