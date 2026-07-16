f862b85a414f734b4f14955c197e0aaecde321ac
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 15:10:31 -0300

    Configure AWS provider and update module settings
    Added AWS provider configuration and updated module to specify providers.

--- env/Homolog/Main.tf
+++ env/Homolog/Main.tf
@@ -1,8 +1,20 @@
-module "prod" {
-    source = "../../infra"
-    
-    cluster_name = "homolog2"
+provider "aws" {
+  region = "us-east-2"
 }
+
+
+module "prod" {
+
+    source = "../../infra"
+
+    cluster_name = "homolog2"
+
+    providers = {
+      aws = aws
+    }
+
+}
+
 
 output "IP_db" {
   value = module.prod.IP
