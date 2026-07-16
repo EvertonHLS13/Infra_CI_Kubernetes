9af34afd81ab940e886850a92a00de61852942c9
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 14:40:48 -0300

    Update EKS module configuration and formatting

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -1,9 +1,10 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-  cluster_name                    = var.cluster_name
-  cluster_version                 = "1.24"
-  cluster_endpoint_public_access  = true
+  cluster_name                   = var.cluster_name
+  cluster_version                = "1.24"
+  cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
@@ -13,8 +14,9 @@ module "eks" {
       min_size     = 1
       max_size     = 10
       desired_size = 3
+
       vpc_security_group_ids = [aws_security_group.ssh_cluster.id]
-      instance_types = ["t2.micro"]
+      instance_types         = ["t2.micro"]
     }
   }
 }
