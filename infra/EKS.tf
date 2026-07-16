79dc94c37283c0e52a5835673cef46c63b929137
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 16:23:23 -0300

    Upgrade EKS cluster version and instance type

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name                   = var.cluster_name
-  cluster_version                = "1.24"
+  cluster_version                = "1.31"
   cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
@@ -16,7 +16,10 @@ module "eks" {
       desired_size = 3
 
       vpc_security_group_ids = [aws_security_group.ssh_cluster.id]
-      instance_types         = ["t2.micro"]
+
+      instance_types = [
+        "t3.micro"
+      ]
     }
   }
 }
