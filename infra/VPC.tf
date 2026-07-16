dfee019a69a64462708433c39dc9056841182b7c
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 14:59:18 -0300

    Update VPC configuration for new region and subnets

--- infra/VPC.tf
+++ infra/VPC.tf
@@ -1,14 +1,34 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
 
   name = "VPC-EKS"
-  cidr = "10.0.0.0/16" #10.0.1.1 - 10.0.255.255
+  cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  database_subnets= ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  
+  azs = [
+    "us-east-2a",
+    "us-east-2b",
+    "us-east-2c"
+  ]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24"
+  ]
+
+  database_subnets = [
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24"
+  ]
+
   create_database_subnet_group           = true
   create_database_subnet_route_table     = true
   create_database_internet_gateway_route = true
@@ -17,4 +37,10 @@ module "vpc" {
   enable_dns_support   = true
 
   enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "Homolog"
+  }
 }
