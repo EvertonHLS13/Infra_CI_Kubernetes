ba5c904ff71b6df8c03ce37417605c6b2bcd3521
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 16:36:21 -0300

    Modify EKS module for private subnets and node sizes
    Updated EKS module configuration to use private subnets and adjusted node group sizes.

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -1,25 +1,62 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
+
+
+  source = "terraform-aws-modules/eks/aws"
+
   version = "~> 20.0"
 
-  cluster_name                   = var.cluster_name
-  cluster_version                = "1.31"
+
+
+  cluster_name = var.cluster_name
+
+
+
+  # Versão suportada atualmente
+  cluster_version = "1.30"
+
+
+
   cluster_endpoint_public_access = true
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnets
+
+
+  vpc_id = module.vpc.vpc_id
+
+
+
+  # IMPORTANTE
+  # Nodes ficam em subnet privada
+
+  subnet_ids = module.vpc.private_subnets
+
+
 
   eks_managed_node_groups = {
-    alura = {
-      min_size     = 1
-      max_size     = 10
-      desired_size = 3
 
-      vpc_security_group_ids = [aws_security_group.ssh_cluster.id]
+
+    alura = {
+
+
+      min_size = 1
+
+      max_size = 3
+
+      desired_size = 2
+
+
 
       instance_types = [
+
         "t3.micro"
+
       ]
+
+
+
     }
+
+
   }
+
+
 }
