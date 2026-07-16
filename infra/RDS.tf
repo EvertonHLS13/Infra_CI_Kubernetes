3255539e35ab7993d5c135c056c8a651c3ae4184
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 16:51:24 -0300

    Update RDS instance configuration parameters

--- infra/RDS.tf
+++ infra/RDS.tf
@@ -1,62 +1,16 @@
 resource "aws_db_instance" "default" {
-
-
-  identifier = "api-go-dev"
-
-
-  allocated_storage = 20
-
-
-
-  engine = "postgres"
-
-
-
-  instance_class = "db.t3.micro"
-
-
-
-  db_name = "api"
-
-
-
-  username = "postgres"
-
-
-
-  password = "rootroot"
-
-
-
-  publicly_accessible = false
-
-
-
-  skip_final_snapshot = true
-
-
-
-  deletion_protection = false
-
-
-
-  db_subnet_group_name = module.vpc.database_subnet_group_name
-
-
-
-  vpc_security_group_ids = [
-
-    aws_security_group.db.id
-
-  ]
-
-
+  allocated_storage      = 10
+  db_name                = "root"
+  engine                 = "postgres"
+  instance_class         = "db.t3.micro"
+  username               = "postgres"
+  password               = "rootroot"
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  db_subnet_group_name   = module.vpc.database_subnet_group_name
+  vpc_security_group_ids = [aws_security_group.db.id]
 }
 
-
-
 output "IP_db" {
-
   value = aws_db_instance.default.address
-
 }
