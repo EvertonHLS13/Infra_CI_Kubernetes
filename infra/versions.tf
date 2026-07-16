48f4aa6043b393b7f493845a29c80057f7da8248
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 14:53:59 -0300

    Create versions.tf with required Terraform providers
    Add Terraform configuration with required providers.

---
+++ infra/versions.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.95"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.13"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3"
+    }
+  }
+}
