ea06fa1792fcb526e75b2db8dd51de0f6d424ceb
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 14:45:53 -0300

    Update Terraform provider versions and configurations

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -1,25 +1,36 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.47"
+      version = "~> 5.95"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
     command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      var.cluster_name
+    ]
   }
 }
 
@@ -27,14 +38,17 @@ resource "kubernetes_service" "LoadBalancer" {
   metadata {
     name = "load-balancer-go-api"
   }
+
   spec {
     selector = {
       nome = "go"
     }
+
     port {
-      port = 8000
+      port        = 8000
       target_port = 8000
     }
+
     type = "LoadBalancer"
   }
 }
