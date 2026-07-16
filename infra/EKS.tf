7b1aafa119100db59096cdea65c0ec8efaf6414e
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 17:38:18 -0300

    Update EKS module configuration and cluster version

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -1,62 +1,42 @@
 module "eks" {
 
-
-  source = "terraform-aws-modules/eks/aws"
-
+  source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
 
-
-  cluster_name = var.cluster_name
-
-
-
-  # Versão suportada atualmente
-  cluster_version = "1.30"
-
+  cluster_name    = var.cluster_name
+  cluster_version = "1.31"
 
 
   cluster_endpoint_public_access = true
 
 
-
   vpc_id = module.vpc.vpc_id
 
 
+  subnet_ids = module.vpc.public_subnets
 
-  # IMPORTANTE
-  # Nodes ficam em subnet privada
 
-  subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
 
 
 
   eks_managed_node_groups = {
 
-
     alura = {
 
-
-      min_size = 1
-
-      max_size = 3
-
+      min_size     = 1
+      max_size     = 3
       desired_size = 2
 
 
-
       instance_types = [
-
         "t3.micro"
-
       ]
-
-
 
     }
 
-
   }
-
 
 }
