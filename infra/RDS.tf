b2149848c5b8914dbdb8197a957b4749456a1a57
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-16 16:37:15 -0300

    Modify RDS instance settings and outputs
    Updated RDS instance configuration with new parameters.

--- infra/RDS.tf
+++ infra/RDS.tf
@@ -1,18 +1,62 @@
 resource "aws_db_instance" "default" {
-  count = 0
 
-  allocated_storage    = 10
-  db_name              = "root"
-  engine               = "postgres"
-  instance_class       = "db.t3.micro"
-  username             = "postgres"
-  password             = "rootroot"
-  skip_final_snapshot  = true
-  publicly_accessible  = false
+
+  identifier = "api-go-dev"
+
+
+  allocated_storage = 20
+
+
+
+  engine = "postgres"
+
+
+
+  instance_class = "db.t3.micro"
+
+
+
+  db_name = "api"
+
+
+
+  username = "postgres"
+
+
+
+  password = "rootroot"
+
+
+
+  publicly_accessible = false
+
+
+
+  skip_final_snapshot = true
+
+
+
+  deletion_protection = false
+
+
+
   db_subnet_group_name = module.vpc.database_subnet_group_name
-  vpc_security_group_ids = [aws_security_group.db.id]
+
+
+
+  vpc_security_group_ids = [
+
+    aws_security_group.db.id
+
+  ]
+
+
 }
 
-output "IP" {
-  value = length(aws_db_instance.default) > 0 ? aws_db_instance.default[0].address : null
+
+
+output "IP_db" {
+
+  value = aws_db_instance.default.address
+
 }
